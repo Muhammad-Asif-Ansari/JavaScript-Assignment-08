dbd16eb10fbe4009a3d0aaf8cd66bede0f281466
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-10-03 20:31:49 +0500

    JavaScript Assignment
    JavaScript Assignment

---
+++ app.js
@@ -0,0 +1,38 @@
+// Ascending order
+function valueSortAsc(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;  // Return the sorted array
+}
+
+// Descending order
+function valueSortDesc(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] < arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;  // Return the sorted array
+}
+
+
+let numbers = [34, 7, 23, 32, 5, 62];
+
+console.log("Original Array: ", numbers);
+
+let ascSorted = valueSortAsc(numbers); 
+console.log("Sorted in Ascending Order: ", ascSorted);
+
+let descSorted = valueSortDesc(numbers);
+console.log("Sorted in Descending Order: ", descSorted);
